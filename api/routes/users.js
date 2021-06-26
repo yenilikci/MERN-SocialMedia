e221fa8cfbfcf4b968beb2edb134224b2b7ad849
yenilikci <m.melihcelik017@outlook.com>
2021-06-26 14:42:51 +0300

    users route içerisinde delete endpoint yazıldı, kullanıcı silinebiliyor

--- api/routes/users.js
+++ api/routes/users.js
@@ -8,7 +8,7 @@ router.get("/", (req, res) => {
 
 //update user
 router.put("/:id", async (req, res) => {
-  if (req.body.userId === req.params.id || req.user.isAdmin) {
+  if (req.body.userId === req.params.id || req.body.isAdmin) {
     //valid
     if (req.body.password) {
       try {
@@ -29,8 +29,21 @@ router.put("/:id", async (req, res) => {
   } else {
     return res.status(403).json("You can update only your account!")
   }
-});
+})
+
 //delete user
+router.delete("/:id", async (req, res) => {
+  if (req.body.userId === req.params.id || req.body.isAdmin) {
+    try {
+      const user = await User.findByIdAndDelete(req.params.id)
+      res.status(200).json("Account has been deleted")
+    } catch (err) {
+      return res.status(500).json(err)
+    }
+  } else {
+    return res.status(403).json("You can delete only your account!")
+  }
+})
 
 //get a user
 
