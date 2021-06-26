cd5057e240c5a641878604b077e9e6335a0ad649
yenilikci <m.melihcelik017@outlook.com>
2021-06-26 16:07:46 +0300

    users route içerisinde get endpoint yazıldı, kullanıcı bilgisi getirilebiliyor

--- api/routes/users.js
+++ api/routes/users.js
@@ -46,8 +46,15 @@ router.delete("/:id", async (req, res) => {
 })
 
 //get a user
-
-//get all users
+router.get('/:id', async (req,res) => {
+  try {
+    const user = await User.findById(req.params.id)
+    const {password,updatedAt,...other} = user._doc
+    res.status(200).json(other)
+  } catch (err) {
+    res.status(500).json(err)
+  }
+})
 
 //follow a user
 
