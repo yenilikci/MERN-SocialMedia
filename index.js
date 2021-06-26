cb59b9a8f3a75cab972221630ca74eb67f9eca13
yenilikci <m.melihcelik017@outlook.com>
2021-06-26 12:37:50 +0300

    ara katman kullanımı ve ilk istekler

--- index.js
+++ index.js
@@ -8,8 +8,22 @@ const morgan = require('morgan')
 
 dotenv.config()
 
+//database connect
 mongoose.connect(process.env.MONGO_URL,{useNewUrlParser:true,useUnifiedTopology:true},() => {
     console.log('Connected to MongoDB')
+})
+
+//middleware
+app.use(express.json()) //body parser
+app.use(helmet())
+app.use(morgan("common"))
+
+app.get('/',(req,res) => {
+    res.send('welcome to homepage')
+})
+
+app.get('/users',(req,res) => {
+    res.send('welcome to user page')
 })
 
 app.listen(8800, () => {
