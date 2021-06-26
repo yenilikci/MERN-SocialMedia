5c86b42a3e37a179038dc4c026280844883984ee
yenilikci <m.melihcelik017@outlook.com>
2021-06-26 14:29:31 +0300

    users route içerisinde put endpoint yazıldı, kullanıcı güncellenebiliyor

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,43 @@
-const router = require('express').Router()
+const router = require("express").Router()
+const bcrypt = require("bcryptjs");
+const User = require('../models/User')
 
-router.get('/',(req,res) => {
-    res.send('hey its user route')
-})
+router.get("/", (req, res) => {
+  res.send("hey its user route")
+});
 
+//update user
+router.put("/:id", async (req, res) => {
+  if (req.body.userId === req.params.id || req.user.isAdmin) {
+    //valid
+    if (req.body.password) {
+      try {
+        const salt = await bcrypt.genSalt(10);
+        req.body.password = await bcrypt.hash(req.body.password, salt)
+      } catch (err) {
+        return res.status(500).json(err)
+      }
+    }
+    try {
+      const user = await User.findByIdAndUpdate(req.params.id, {
+        $set: req.body,
+      })
+      res.status(200).json("Account has been updated")
+    } catch (err) {
+      return res.status(500).json(err)
+    }
+  } else {
+    return res.status(403).json("You can update only your account!")
+  }
+});
+//delete user
+
+//get a user
+
+//get all users
+
+//follow a user
+
+//unfollow a user
 
 module.exports = router
