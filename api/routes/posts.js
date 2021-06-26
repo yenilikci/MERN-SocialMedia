a4aca385a93f658dd1b1fbd3c731e829f6514581
yenilikci <m.melihcelik017@outlook.com>
2021-06-26 22:18:35 +0300

    posts router içerisinde put endpoint oluşturuldu, kullanıcılar gönderilerini güncelleyebiliyorlar

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -12,12 +12,24 @@ router.post("/", async (req, res) => {
   }
 });
 //update a post
-
+router.put("/:id", async (req, res) => {
+  try {
+    const post = await Post.findOne({ _id: req.params.id });
+    if (post.userId === req.body.userId) {
+      await post.updateOne({ $set: req.body });
+      res.status(200).json("The post has been updated");
+    } else {
+      res.status(403).json("You can update only your post");
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 //delete a post
 
 //like a post
 
-//get a post 
+//get a post
 
 //get timeline posts
 
