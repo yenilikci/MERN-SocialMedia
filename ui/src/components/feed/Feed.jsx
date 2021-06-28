a6e00bdba63e0296936964938a9bfbf536f64b78
yenilikci <m.melihcelik017@outlook.com>
2021-06-28 18:28:04 +0300

    Post verileri dummy datadan dinamik olarak çekildi

--- ui/src/components/feed/Feed.jsx
+++ ui/src/components/feed/Feed.jsx
@@ -1,19 +1,18 @@
-import React from 'react'
-import Post from '../post/Post'
-import Share from '../share/Share'
-import "./feed.css"
+import React from "react";
+import Post from "../post/Post";
+import Share from "../share/Share";
+import "./feed.css";
+import { Posts } from "../../dummyData";
 
 export default function Feed() {
-    return (
-        <div className="feed">
-            <div className="feedWrapper">
-                <Share/>
-                <Post/>
-                <Post/>
-                <Post/>
-                <Post/>
-            </div>
-        </div>
-    )
+  return (
+    <div className="feed">
+      <div className="feedWrapper">
+        <Share />
+        {Posts.map((p) => (
+          <Post key={p.id} post={p}/>
+        ))}
+      </div>
+    </div>
+  );
 }
-    
